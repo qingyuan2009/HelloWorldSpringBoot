37a2f66986fe0885750aaee7721bce6d98deae6a
zhouru <wshuuei@hotmail.com>
2020-01-27 16:00:33 +0800

    commit

--- src/main/java/com/rui/demo/HelloWorldSpringBootApplication.java
+++ src/main/java/com/rui/demo/HelloWorldSpringBootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @SpringBootApplication 包含了 @EnableAutoConfiguration
 public class HelloWorldSpringBootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rui/demo/controller/PropertyController.java
@@ -0,0 +1,52 @@
+package com.rui.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ConfigurationProperties(prefix="book")
+public class PropertyController {
+	
+	//方法1
+	@Value("${book.author}")
+	private String author2;
+		
+	@Value("${book.name}")
+	private String name2;
+	
+	@RequestMapping("/property")
+	@ResponseBody	
+	public String showInfo() {
+		return author2 + ":" + name2 + ";" + author + ":" + name ;
+		
+	}	
+	
+	//方法2
+	//@Value("${book.author}")     不需要使用@Value
+	private String author;
+		
+	//@Value("${book.name}")
+	private String name;	
+		
+	//但必须实现Get， Set 方法
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+		
+	public String getName() {
+		return name;
+	}
+	
+	public void setname(String name) {
+		this.name = name;
+	}	
+
+}
