d158eaab7a6e318fb3ed541e2888d8cd4a999571
zhouru <wshuuei@hotmail.com>
2020-01-27 21:23:16 +0800

    commit

--- src/test/java/com/rui/demo/HelloWorldSpringBootApplicationTests.java
+++ src/test/java/com/rui/demo/HelloWorldSpringBootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.rui.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
+import com.rui.demo.controller.IndexController;
+
+import junit.framework.TestCase;
+
+//IndexController = 需要测试的类
+@SpringBootTest(classes=IndexController.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 class HelloWorldSpringBootApplicationTests {
+	
+	@Autowired
+	private IndexController indexController;
 
 	@Test
-	void contextLoads() {
+	public void test1() {
+		
+		TestCase.assertEquals(this.indexController.first(), "Hello World!");
 	}
-
 }
